b77dd5c398e0290fd787142a0edf17abafeff249
Octavio Fariña <octaviofarinia@gmail.com>
2023-04-29 12:38:01 -0300

    project initialization

---
+++ src/main/java/com/octavio/laboratoriotp5/Laboratoriotp5Application.java
@@ -0,0 +1,13 @@
+package com.octavio.laboratoriotp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratoriotp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratoriotp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/octavio/laboratoriotp5/Laboratoriotp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octavio.laboratoriotp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratoriotp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
